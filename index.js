e836023f3a7b07ec007cb7ce1e11234b92e1d798
Manjeet Singh <manjeetsingh.makaut@gmail.com>
2024-08-10 14:17:08 +0530

    worked on the working

---
+++ index.js
@@ -0,0 +1,12 @@
+let buttton1=document.getElementById("cric");
+
+buttton1.addEventListener('click',function(){
+    window.location.href= "cricket.html";
+})
+
+
+let buttton2=document.getElementById("foot");
+
+buttton2.addEventListener('click',function(){
+    window.location.href= "football.html";
+})
